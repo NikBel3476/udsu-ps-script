f9c9e6c96986b1092e7dc0bf8240a161d7914a34
NikBel3476 <nikita.belkin3476@gmail.com>
2023-07-05 13:07:59 +0400

    chore: remove unused code

--- run_as_admin.ps1
+++ run_as_admin.ps1
@@ -136,28 +136,6 @@ Function Remove-User {
     }
 }
 
-# TODO: try it
-# function Remove-LocalUserCompletely {
-
-#     Param(
-#         [Parameter(ValueFromPipelineByPropertyName)]
-#         $Name
-#     )
-
-#     process {
-#         $user = Get-LocalUser -Name $Name -ErrorAction Stop
-
-#         # Remove the user from the account database
-#         Remove-LocalUser -SID $user.SID
-
-#         # Remove the profile of the user (both, profile directory and profile in the registry)
-#         Get-CimInstance -Class Win32_UserProfile | ? SID -eq $user.SID | Remove-CimInstance
-#     }
-# }
-
-# Example usage:
-# Remove-LocalUserCompletely -Name 'myuser'
-
 Function Test-RegistryValue {
     param(
         [Alias("PSPath")]
